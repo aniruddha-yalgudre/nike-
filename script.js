bf28639dce19f5eea2c2375c544cf0708f3ac9c4
aniruddha <aniruddhayalgudre@gmail.com>
2024-02-01 16:07:50 +0530

    fist commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//selction of element WITH  html and css
+
+
+var a = document.querySelector("h1")
+
+
+
+a.addEventListener("click",function(){
+
+    a.innerHTML = "aniruddha"
+    a.style.color="red"
+    a.style.backgroundColor="black"
+
+
+
+})
+    
+
+    //event listener
+
+    
